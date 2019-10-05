172cd1cb7d6cf0f89922266512de0032622b0217
NishiumiRyutaro <dragon.west@icloud.com>
2019-10-05 18:51:28 +0900

    botprogram

--- bot.py
+++ bot.py
@@ -1,15 +1,26 @@
-
 import discord
-import asyncio
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    asyncio.ensure_future(greeting_gm())
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
 
-async def greeting_gm():
-    await client.send_message('おはよう')
-    await asyncio.sleep(10)
+@client.event
+async def on_message(message):
+    # 「おはよう」で始まるか調べる
+    if message.content.startswith("古戦場"):
+        # 送り主がBotだった場合反応したくないので
+        if client.user != message.author:
+            # メッセージを書きます
 
-client.run("NjI5Njk3ODExODA5ODI4ODk0.XZdh-g.Qq7xokdqTwK0lUfxx17opyWRQx4")
+            m = "http://game.granbluefantasy.jp/#top" + message.author.name + "さん！"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await message.channel.send(m)
+
+
+            # token にDiscordのデベロッパサイトで取得したトークンを入れてください
+client.run("NjI5Njk3ODExODA5ODI4ODk0.XZhkpw.AFw-cnUncYQlt0-XFZx70BfAH44")
